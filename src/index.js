49913b1266371ee7d286ba20caa2d5d2750b2c82
Ajinkya Gokule <ajinkyagokule@gmail.com>
2023-10-20 21:14:50 -0700

    skeleton code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+const extensionHeader = document.getElementById('extension-header');
+
+// render exntension header with hello world
+extensionHeader.innerHTML = 'Hello World!';
 
 // input: string
 // description: remove html tags from string
 // output: string
-
 function removeHtmlTags(pageSource) {
     return pageSource.replace(/<[^>]*>/g, '');
 }

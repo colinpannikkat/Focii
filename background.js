f05cc4245b87c6dde19bc7fe527ff4e67cf69bd9
aj-arts <ajinkyagokule@gmail.com>
2023-10-22 18:17:34 -0700

    working on blockedHTML

--- background.js
+++ background.js
@@ -1,5 +1,89 @@
 // const keywordInput = document.getElementById('keyword-input');
 
+const blockedHtml = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Blocked</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+        .blocked-container {
+            display: none;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+            margin: 10% auto;
+            max-width: 600px;
+            opacity: 0;
+            transform: translateY(-20px);
+            transition: transform 0.5s, opacity 0.5s;
+        }
+        h1 {
+            color: #f44336;
+            font-size: 36px;
+            margin: 0 0 20px;
+            font-weight: bold;
+        }
+        p {
+            font-size: 20px;
+            color: #333;
+            margin: 10px 0;
+            line-height: 1.6;
+        }
+        .subtle-text {
+            font-size: 18px;
+            color: #555;
+            font-style: italic;
+        }
+        body::before {
+            content: '';
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><rect width="100%" height="100%" fill="#f44336"/><circle cx="50" cy="50" r="30" fill="#4285f4"/></svg>');
+            z-index: -1;
+            opacity: 0.2;
+        }
+    </style>
+</head>
+<body>
+    <div class="blocked-container">
+        <h1>Oops, You've Hit a Roadblock!</h1>
+        <p>This website doesn't align with your current mission. It's time to refocus and conquer your goals.</p>
+        <p class="subtle-text">Focii Extension</p>
+    </div>
+    <script>
+        window.onload = function() {
+            const blockedContainer = document.querySelector(".blocked-container");
+            blockedContainer.style.display = "block";
+            let opacity = 0;
+            const fadeIn = setInterval(function() {
+                if (opacity < 1) {
+                    opacity += 0.05;
+                    blockedContainer.style.opacity = opacity;
+                    blockedContainer.style.transform = translateY(0px);
+                } else {
+                    clearInterval(fadeIn);
+                }
+            }, 50);
+        }
+    </script>
+</body>
+</html>
+`
+
 function stringToList(inputString) {
     const items = inputString.split(',').map(item => item.trim());
   
@@ -54,7 +138,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                         target: {tabId: tabs[0].id},
                         function: () => {
                             document.documentElement.innerHTML = "";
-                            document.documentElement.innerText = "This website is not similar to the keywords you have inputted. Please try again.";
+                            document.documentElement.innerHTML = "<h1>Website Blocked</h1>";
                         }
                     });
                 });
